8a0a92346ec841ef8d010f0048eca6412731e30c
nxa127 <nxa127@gmail.com>
2021-11-30 15:08:55 +0700

    agora for IOS

--- App.tsx
+++ App.tsx
@@ -1,112 +1,144 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {StyleSheet, View, TextInput, Button, SafeAreaView} from 'react-native';
+import RtcEngine from 'react-native-agora';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+type State = {
+  appId: string;
+  token: string;
+  channelName: string;
+  joinSucceed: boolean;
+  openMicrophone: boolean;
+  enableSpeakerphone: boolean;
+  peerIds: number[];
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  top: {
+    width: '100%',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
 
-export default App;
+export default class App extends Component<{}, State, any> {
+  _engine?: RtcEngine;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      channelName: 'channel_test_01',
+      joinSucceed: false,
+      appId: 'a844e635081a492098c29afe0db517dc',
+      token:
+        '006a844e635081a492098c29afe0db517dcIAAHnvUauzb7A4IPk+4YSfbdnqcA+7BEaCT7zeNED46VoT54ZtkAAAAAEABgg+xaZAanYQEAAQBkBqdh',
+      openMicrophone: true,
+      enableSpeakerphone: true,
+      peerIds: [],
+    };
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  init = async () => {
+    const {appId} = this.state;
+    this._engine = await RtcEngine.create(appId);
+
+    await this._engine.enableAudio();
+
+    this._engine.addListener('UserJoined', (uid, elapsed) => {
+      console.log('UserJoined', uid, elapsed);
+      const {peerIds} = this.state;
+      if (peerIds.indexOf(uid) === -1) {
+        this.setState({
+          peerIds: [...peerIds, uid],
+        });
+      }
+    });
+
+    this._engine.addListener('UserOffline', (uid, reason) => {
+      console.log('UserOffline', uid, reason);
+      const {peerIds} = this.state;
+      this.setState({
+        peerIds: peerIds.filter(id => id !== uid),
+      });
+    });
+
+    this._engine.addListener('JoinChannelSuccess', (channel, uid, elapsed) => {
+      console.log('JoinChannelSuccess', channel, uid, elapsed);
+      this.setState({
+        joinSucceed: true,
+      });
+    });
+  };
+
+  _joinChannel = async () => {
+    const {token, channelName} = this.state;
+    console.log('joinChannel', token, channelName);
+    await this._engine?.joinChannel(token, channelName, null, 0);
+  };
+
+  _switchMicrophone = () => {
+    const {openMicrophone} = this.state;
+    this._engine
+      ?.enableLocalAudio(!openMicrophone)
+      .then(() => {
+        this.setState({openMicrophone: !openMicrophone});
+      })
+      .catch(err => {
+        console.warn('enableLocalAudio', err);
+      });
+  };
+
+  _switchSpeakerphone = () => {
+    const {enableSpeakerphone} = this.state;
+    this._engine
+      ?.setEnableSpeakerphone(!enableSpeakerphone)
+      .then(() => {
+        this.setState({enableSpeakerphone: !enableSpeakerphone});
+      })
+      .catch(err => {
+        console.warn('setEnableSpeakerphone', err);
+      });
+  };
+
+  _leaveChannel = async () => {
+    await this._engine?.leaveChannel();
+    this.setState({peerIds: [], joinSucceed: false});
+  };
+
+  render() {
+    const {channelName, joinSucceed, openMicrophone, enableSpeakerphone} =
+      this.state;
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.top}>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => this.setState({channelName: text})}
+            placeholder={'Channel Name'}
+            value={channelName}
+          />
+          <Button
+            onPress={joinSucceed ? this._leaveChannel : this._joinChannel}
+            title={`${joinSucceed ? 'Leave' : 'Join'} channel`}
+          />
+        </View>
+        <Button
+          onPress={this._switchMicrophone}
+          title={`Microphone ${openMicrophone ? 'on' : 'off'}`}
+        />
+        <Button
+          onPress={this._switchSpeakerphone}
+          title={enableSpeakerphone ? 'Speakerphone' : 'Earpiece'}
+        />
+      </SafeAreaView>
+    );
+  }
+}
